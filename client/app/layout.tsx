48c254a9ed4f62069dcb69a5ee48636f7dc513c3
Priyanshu Verma <priyanshuverma@outlook.in>
2024-09-07 14:58:38 +0530

    metadata added

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -25,6 +25,18 @@ const fontBody = IBM_Plex_Mono({
 export const metadata: Metadata = {
   title: "Realtime Stocks Chart",
   description: "Realtime Stocks Chart with fluvio",
+  metadataBase: new URL("https://stockviz.vercel.app"),
+  openGraph: {
+    images: ["https://stockviz.vercel.app/stockviz.png"],
+    type: "website",
+    siteName: "StockViz - Realtime Stocks Chart",
+  },
+  twitter: {
+    images: ["https://stockviz.vercel.app/stockviz.png"],
+    site: "https://stockviz.vercel.app",
+    title: "StockViz - Realtime Stocks Chart",
+    card: "summary_large_image",
+  },
 };
 
 export default async function RootLayout({
